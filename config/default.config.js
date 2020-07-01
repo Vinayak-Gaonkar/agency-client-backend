3b4e0571884b029b1a2d0d5a132e4c8c2f83c5c1
vinayak_gaonkar <vinayak.gaonkar@mobinius.com>
2020-07-01 09:49:05 +0530

    done | config

--- config/default.config.js
+++ config/default.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 3004,
-  host:"https://morning-woodland-32458.herokuapp.com/",
+  host:"morning-woodland-32458.herokuapp.com",
   DB_URL:"mongodb+srv://vinayak:vinayak123@cluster0.vo4hu.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
   jwt_secret: "M0nsteR+M!nd",
   jwt_expiration_in_seconds: 36000,
